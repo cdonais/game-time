f8db0c7fca18302667d2ff3d3e8c957fb1a05795
Madison Mulligan <madisonmulligan97@gmail.com>
2022-09-14 13:36:34 -0400

    Added components and started dashboard

---
+++ game-time/src/components/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <section id="Dashboard">
+            <div className="container">
+                <div className="row">
+                    <div className='col-lg-7 d-flex align-items-center'>
+                    </div>
+                    <h2>Dashboard</h2>
+                    </div>
+                </div>
+        </section>
+    )
+}
+export default Dashboard;
