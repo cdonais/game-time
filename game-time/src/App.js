ad85ec9783712e22956a91ed5d2b433a1addc11e
Madison Mulligan <madisonmulligan97@gmail.com>
2022-09-14 15:08:53 -0400

    Worked in nav and app.js

--- game-time/src/App.js
+++ game-time/src/App.js
@@ -1,8 +1,42 @@
-import React, from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
-  
+  const [navLinks] = useState([
+    {
+      name: 'Home'
+    },
+    {
+      name: 'Dashboard'
+    },
+    {
+      name: 'Find Game'
+    },
+    {
+      name: 'Login'
+    },
+    {
+      name: 'Signup'
+    }
+
+  ]);
+
+  const [currentNavLink] = useState(navLinks[0]);
+  const renderLink = navLinks.filter((link) => link.name ===link);
+
+  return (
+    <div>
+      <nav
+      navLinks={navLinks}
+      setcurrentNavLink
+      currentNavLink={currentNavLink}
+      renderLink={renderLink}>
+      </nav>
+      <nav></nav>
+      <main></main>
+    </div>
+  )
 }
 
 export default App;
--- game-time/src/components/Nav/index.js
+++ game-time/src/components/Nav/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function Nav(props) {
+    const { navLinks = [], 
+    setCurrentNavLink,
+    currentNavLink,
+} = props;
     
 return (
     <header className="container-fluid">
@@ -18,7 +22,36 @@ return (
                         <span className="toggler-icon middle-bar"></span>
                         <span className="toggler-icon bottom-bar"></span>
                     </button>
-                    
+                    <div className="collapse navbar-collapse"
+                        id="navbarToggleExternalContent"></div>
+                        <ul className="navbar-nav">
+                            {navLinks.map((navLink) => (
+                                <li key={navLink.name}>
+                                 <span>
+                                    <a
+                                    className={`nav-item ${
+                                    currentNavLink.name === navLink.name && "navActive"
+                                     }`}
+                                    href={"#" + navLink.name}
+                                    onClick={() => {
+                                    if (navLink.name === "Dashboard") {
+                                     } else {
+                                      setCurrentNavLink(navLink);
+                                    }
+                                    if (navLink.name === "Find Game") {
+                                    }                     
+                                      else {
+                                      setCurrentNavLink(navLink);
+                                     }
+                                      setCurrentNavLink(navLink);
+                                     }}
+                                     >
+                                     {navLink.name}
+                                     </a>
+                                    </span>
+                                </li>
+                            ))}
+                        </ul>
                 </nav>
             </div>
         </section>
