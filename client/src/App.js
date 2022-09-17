5c14829bb6929ac4dcaebe9bfff481cc375a3ae6
Carly Donais <carly.donais@gmail.com>
2022-09-17 12:01:39 -0400

    updated login signup find game

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,60 @@
-import React, { useState } from "react";
-import "./App.css";
-//import Nav from './components/Nav';
+// import React, {useState} from 'react';
+// import './App.css';
+// //import Nav from './components/Nav';
+
+// function App() {
+//   const [navLinks] = useState([
+//     {
+//       name: 'Home'
+//     },
+//     {
+//       name: 'Dashboard'
+//     },
+//     {
+//       name: 'Find Game'
+//     },
+//     {
+//       name: 'Login'
+//     },
+//     {
+//       name: 'Signup'
+//     }
+
+//   ]);
+
+//   const [currentNavLink] = useState(navLinks[0]);
+//   const renderLink = navLinks.filter((link) => link.name ===link);
+
+//   return (
+//     <div>
+//       <nav
+//       navLinks={navLinks}
+//       setcurrentNavLink
+//       currentNavLink={currentNavLink}
+//       renderLink={renderLink}>
+//       </nav>
+//       <nav></nav>
+//       <main></main>
+//     </div>
+//   )
+// }
+
+// export default App;
+import React from 'react';
+import Nav from './components/Nav';
+import FindGame from './components/Find-Game';
+import Login from './components/Log-in';
+import Signup from './components/Sign-up';
 
 function App() {
-  const [navLinks] = useState([
-    {
-      name: "Home",
-    },
-    {
-      name: "Dashboard",
-    },
-    {
-      name: "Find Game",
-    },
-    {
-      name: "Login",
-    },
-    {
-      name: "Signup",
-    },
-  ]);
-
-  const [currentNavLink] = useState(navLinks[0]);
-  const renderLink = navLinks.filter((link) => link.name === link);
-
   return (
     <div>
-      <nav
-        navLinks={navLinks}
-        setcurrentNavLink
-        currentNavLink={currentNavLink}
-        renderLink={renderLink}
-      ></nav>
-      <nav></nav>
-      <main></main>
+      <Nav></Nav>
+      <main>
+      <Login></Login>
+      <Signup></Signup>
+        <FindGame></FindGame>
+      </main>
     </div>
   );
 }
