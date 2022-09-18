960631f17390b92524671ed07443e8f13c8317df
Joseph Darville <joseph.darville9@gmail.com>
2022-09-17 20:46:53 -0500

    Got React to render html

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,43 @@
-// import React, {useState} from 'react';
-// import './App.css';
-// //import Nav from './components/Nav';
-
-// function App() {
-//   const [navLinks] = useState([
-//     {
-//       name: 'Home'
-//     },
-//     {
-//       name: 'Dashboard'
-//     },
-//     {
-//       name: 'Find Game'
-//     },
-//     {
-//       name: 'Login'
-//     },
-//     {
-//       name: 'Signup'
-//     }
-
-//   ]);
-
-//   const [currentNavLink] = useState(navLinks[0]);
-//   const renderLink = navLinks.filter((link) => link.name ===link);
-
-//   return (
-//     <div>
-//       <nav
-//       navLinks={navLinks}
-//       setcurrentNavLink
-//       currentNavLink={currentNavLink}
-//       renderLink={renderLink}>
-//       </nav>
-//       <nav></nav>
-//       <main></main>
-//     </div>
-//   )
-// }
-
-// export default App;
-import React from 'react';
-import Nav from './components/Nav';
-import FindGame from './components/Find-Game';
-import Login from './components/Log-in';
-import Signup from './components/Sign-up';
+import React, { useState } from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import Dashboard from "./components/Dashboard";
+import FindGame from "./components/Find-Game";
+import Footer from "./components/Footer";
+import Login from "./components/Log-in";
+import Signup from "./components/Sign-up";
 
 function App() {
+  const [navLinks] = useState([
+    {
+      name: "Home",
+    },
+    {
+      name: "Dashboard",
+    },
+    {
+      name: "Find Game",
+    },
+    {
+      name: "Login",
+    },
+    {
+      name: "Signup",
+    },
+  ]);
+
+  const [currentNavLink] = useState(navLinks[0]);
+  const renderLink = navLinks.filter((link) => link.name === link);
+
   return (
     <div>
-      <Nav></Nav>
-      <main>
-      <Login></Login>
-      <Signup></Signup>
-        <FindGame></FindGame>
-      </main>
+      <Nav
+        navLinks={navLinks}
+        setcurrentNavLink
+        currentNavLink={currentNavLink}
+        renderLink={renderLink}
+      ></Nav>
+      <main></main>
     </div>
   );
 }
