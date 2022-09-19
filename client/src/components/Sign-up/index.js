11cd7f898e71180c6ad3623e8108bf16f8894a08
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-09-19 16:39:22 -0400

    edited text and submit buttons

--- client/src/components/Sign-up/index.js
+++ client/src/components/Sign-up/index.js
@@ -1,33 +1,32 @@
-import React from 'react';
+import React from "react";
 
 function Signup() {
-    return (
-        <section className='signupform'>
-            <h2>Sign Up:</h2>
-          <form>
-            <label>
-                Email:
-                <input type="text" name="email" />
-            </label>
-            <label>
-                Username:
-                <input type="text" name="username" />
-            </label>
-            <label>
-                Password:
-                <input type="text" name="password" />
-            </label>
-            <section className='skill'>
-              <h3>Skill Level:</h3>  
-             <input type="radio" value="Beginner" name="skill"/> Beginner
-             <input type="radio" value="Intermediate" name="skill"/> Intermediate   
-             <input type="radio" value="Advanced" name="skill"/> Advanced 
-             <input type="submit" value="Submit" />  
-   
-            </section>
-            </form>
+  return (
+    <section className="signupform">
+      <h2>Sign Up:</h2>
+      <form>
+        <label>
+          Email:
+          <input type="text" name="email" />
+        </label>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="text" name="password" />
+        </label>
+        <section className="skill">
+          <h3>Skill Level:</h3>
+          <input type="radio" value="Beginner" name="skill" /> Beginner
+          <input type="radio" value="Intermediate" name="skill" /> Intermediate
+          <input type="radio" value="Advanced" name="skill" /> Advanced
+          <button type="submit">Submit</button>
         </section>
-    );
+      </form>
+    </section>
+  );
 }
 
 export default Signup;
